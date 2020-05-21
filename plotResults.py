6bd8b0715e22de68951f501c88986cfd48f8abba
FrancescaPerin <36476749+FrancescaPerin@users.noreply.github.com>
2020-05-21 16:39:45 +0200

    Added small script to plot json data

---
+++ plotResults.py
@@ -0,0 +1,41 @@
+import json
+from argparse import ArgumentParser
+from matplotlib import pyplot as plt
+
+def main(args):
+
+	# Load data
+	with open(args.data, "rt") as f:
+		data = json.load(f)
+
+	# Store results separately
+	results = data["results"]
+
+	# Get x axis (epochs) values
+	x = list(range(len(results["avg"])))
+
+	# Plot cumulative average alone
+	plt.figure()
+
+	plt.plot(x, results["avg"])
+
+	plt.title("Cumulative Average Reward")
+	plt.xlabel("Time-Step")
+	plt.ylabel("Avergage reward at time-step")
+
+	plt.show()
+
+if __name__ == "__main__":
+
+	parser = ArgumentParser()
+
+	parser.add_argument(
+		"--data",
+		type = lambda x : x + ".json" if not x.endswith(".json") else x,
+		default = "results.json",
+		help = "Name of json file from which to get results"
+	)
+
+	args = parser.parse_args()
+
+	main(args)
